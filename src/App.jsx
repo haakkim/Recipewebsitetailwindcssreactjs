d927771943016d1ee14a094067755f87e0aa3379
haakkim <hakamali1537@gmail.com>
2024-02-02 10:33:23 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import TopNav from './components/TopNav'
+import Featured from './components/Featured'
+import Delivery from './components/Delivery'
+import TopPics from './components/TopPics'
+import Meal from './components/Meal'
+import Categories from './components/Categories'
+import NewsLetter from './components/NewsLetter'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+     <div>
+      <TopNav/>
+      <Featured/>
+      <Delivery/>
+      <TopPics/>
+      <Meal/>
+      <Categories/>
+      <NewsLetter/>
+      <Footer/>
+      
+
+
+     </div>
+  )
+}
+
+export default App
